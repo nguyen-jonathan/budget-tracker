276b5988e02fd83b183545b6a9926d32ba57d9d7
Jonathan Nguyen <njonathan990@gmail.com>
2022-09-11 23:52:39 -0500

    add budget component

--- src/App.js
+++ src/App.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Budget from './components/Budget/Budget';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="mt-3">Budget Tracker</h1>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <Budget />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
