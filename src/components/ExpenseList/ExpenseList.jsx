d4db654c123cf0225281a50dc80d8e6e4d794d6f
Jonathan Nguyen <njonathan990@gmail.com>
2022-09-12 20:33:32 -0500

    appContext to ExpenseList

--- src/components/ExpenseList/ExpenseList.jsx
+++ src/components/ExpenseList/ExpenseList.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import ExpenseItem from '../ExpenseItem/ExpenseItem';
+import {AppContext} from '../../context/AppContext';
 
 const ExpenseList = () => {
   // hard code //
-  const expenses = [
-    {id: 123, name: 'Shopping', amount: 50},
-    {id: 123, name: 'Gas', amount: 70},
-    {id: 123, name: 'Bills', amount: 200},
-    {id: 123, name: 'Car Rental', amount: 350},
-  ];
+  // const expenses = [
+  //   {id: 123, name: 'Shopping', amount: 50},
+  //   {id: 123, name: 'Gas', amount: 70},
+  //   {id: 123, name: 'Bills', amount: 200},
+  //   {id: 123, name: 'Car Rental', amount: 350},
+  // ];
+
+  const {expenses} = useContext(AppContext);
+
   return (
     <ul className="list-group">
       {expenses.map((expense) => (
