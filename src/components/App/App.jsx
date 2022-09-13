dbbfc813189fbac7334411a7930b2a6e4d68cc02
Jonathan Nguyen <njonathan990@gmail.com>
2022-09-12 20:25:17 -0500

    AppProvider and AppContext

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,35 +5,38 @@ import Balance from '../Balance/Balance';
 import Expense from '../Expense/Expense';
 import ExpenseList from '../ExpenseList/ExpenseList';
 import AddExpenseForm from '../AddExpenseForm/AddExpenseForm';
+import {AppProvider} from '../AppContext';
 
 const App = () => {
   return (
-    <div className="container">
-      <h1 className="mt-3">Budget Tracker</h1>
-      <div className="row mt-3">
-        <div className="col-sm">
-          <Budget />
+    <AppProvider>
+      <div className="container">
+        <h1 className="mt-3">Budget Tracker</h1>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <Budget />
+          </div>
+          <div className="col-sm">
+            <Balance />
+          </div>
+          <div className="col-sm">
+            <Expense />
+          </div>
         </div>
-        <div className="col-sm">
-          <Balance />
+        <h3 className="mt-3">Expense Transactions</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <ExpenseList />
+          </div>
         </div>
-        <div className="col-sm">
-          <Expense />
+        <h3 className="mt-3">Add Expense</h3>
+        <div className="mt-3">
+          <div className="col-sm">
+            <AddExpenseForm />
+          </div>
         </div>
       </div>
-      <h3 className="mt-3">Expense Transactions</h3>
-      <div className="row mt-3">
-        <div className="col-sm">
-          <ExpenseList />
-        </div>
-      </div>
-      <h3 className="mt-3">Add Expense</h3>
-      <div className="mt-3">
-        <div className="col-sm">
-          <AddExpenseForm />
-        </div>
-      </div>
-    </div>
+    </AppProvider>
   );
 };
 
