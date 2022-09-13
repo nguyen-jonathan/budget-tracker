f0db438caf40401607edc4f8cb59843889afb0a6
Jonathan Nguyen <njonathan990@gmail.com>
2022-09-12 22:40:41 -0500

    alert box color condition for positive/negative balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -8,8 +8,11 @@ const Balance = () => {
     return (total = total + item.amount);
   }, 0);
 
+  // card color changes whether positive or negative number //
+  const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+
   return (
-    <div className="alert alert-success">
+    <div className={`alert ${alertType}`}>
       <span>Balance: ${budget - totalExpenses}</span>
     </div>
   );
